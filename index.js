cf7e8521a398d4c8405129961e3dff75654f6cdc
SarahJD <sarahjdudek@gmail.com>
2021-02-22 08:49:41 +0100

    install packages, loggind middleware, fetch function and excelToJson conversion

--- index.js
+++ index.js
@@ -1,4 +1,68 @@
-const app = require('./src/app');
+const express = require('express'),
+morgan = require('morgan'), // logging middleware
+fs = require('fs'),
+path = require('path'),
+fetch = require('node-fetch'),
+excelToJson = require('convert-excel-to-json');
+
+const app = express();
+
+// Log to the file access.log
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+app.use(morgan('common', { stream: accessLogStream }));
+
+app.use(express.static('public'));
+
+// // for error handling
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500.send('something broke!'));
+// });
+
+
+// Fetch xlsx-file from rki.de
+  const url = 'https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquotenmonitoring.xlsx?__blob=publicationFile';
+  const rkiFile = `./rki-data.xlsx`;
+  const re = /Gesamt/;
+
+  async function download() {
+    const response = await fetch(url);
+    const buffer = await response.buffer();
+    fs.writeFile(rkiFile, buffer, () => 
+      console.log('finished downloading!'));
+  }
+
+  download();
+
+// Convert xlsx-file to JSON
+  const result = excelToJson({
+    sourceFile: rkiFile,
+    header:{
+        rows: 1
+    }
+  });
+
+  let key;
+  for(key in result){
+    const found = key.match(re);
+    console.log(found);
+  }
+  
+
+  
+
+// // Endpoint 
+// app.get('', (req, res) => {
+//   Movies.find()
+//     .then((movies) => {
+//       res.status(201).json(movies);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).send('Error: ' + err);
+//     });
+// });
+
 
 const PORT = 4000;
 
